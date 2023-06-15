c0b6419e7794ae502fc0f3db310ba201659efa8b
xxx-Event <s2119@e-mirim.hs.kr>
2023-06-15 09:58:06 +0900

    메모라이제이션 적용한 피보나치수열 연습

---
+++ RecursionPrac/FibonacciAdv.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecursionPrac32
+{
+    internal class FibonacciAdv
+    {
+        private static Dictionary<int, long> memo= new Dictionary<int, long>();
+
+        public long Get(int n)
+        {
+            if(n < 0) { return 0; }
+            if(n == 1) { return 1; }
+
+            if(memo.ContainsKey(n)) { return memo[n]; }
+            else
+            {
+                long value = Get(n - 2) + Get(n - 1);
+                memo[n] = value;
+                return value;
+            }
+        }
+    }
+}
--- RecursionPrac/Program.cs
+++ RecursionPrac/Program.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RecursionPrac
+namespace RecursionPrac32
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Fibonacci fibo = new Fibonacci();
+            //Console.WriteLine(fibo.Get(50));
+
+            FibonacciAdv fiboAdv = new FibonacciAdv();
+            Console.WriteLine(fiboAdv.Get(50000));
         }
     }
 }
