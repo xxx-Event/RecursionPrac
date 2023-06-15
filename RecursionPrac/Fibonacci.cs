dd41184d8ee6fce1c2a64af15d864440a3628d18
xxx-Event <s2119@e-mirim.hs.kr>
2023-06-15 09:58:57 +0900

    정석적인 피보나치 수열 수현

---
+++ RecursionPrac/Fibonacci.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RecursionPrac32
+{
+    internal class Fibonacci
+    {
+        public long Get(int n)
+        {
+            Console.Write(n + " ");
+            if (n < 0) { return 0; }
+            if (n == 1) { return 1;}
+            return Get(n - 2) + Get(n - 1);
+        }
+    }
+}
